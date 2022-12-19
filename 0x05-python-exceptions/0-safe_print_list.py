e269d42521bde329dde443cf036e529f9ef667ae
dannyntale10 <dannyntale10@gmail.com>
2022-12-19 13:39:13 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/user/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            return(x)
+    except:
+        if x > my_list:
+            return(x)
