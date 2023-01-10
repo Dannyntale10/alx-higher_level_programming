5351ddc601d8f4455f2d0edc6031a9972a411a46
dannyntale10 <dannyntale10@gmail.com>
2023-01-10 11:06:14 -0800

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+"""Defines a class Student"""
 
 
 class Student:
-    """ Class to create student instances """
+    """Represents the class"""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initializes the data of the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        """retrieves a dict representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_list = []
+            for i in attrs:
+                if i in self.__dict__:
+                    new_list.append(i)
+            return {x: self.__dict__[x] for x in new_list}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for i, j in json.items():
+            self.__dict__[i] = 
