d9adac13fc7e2fc9dc4ebad57c75819c20132362
dannyntale10 <dannyntale10@gmail.com>
2023-01-10 10:09:26 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 def write_file(filename="", text=""):
     """A function that writes a string to a tex tfile(UTF8) and
-    and returns the number of characters written
+        returns the number of characters written
     """
 
     with open(filename, 'w', encoding='utf-8') as f:
