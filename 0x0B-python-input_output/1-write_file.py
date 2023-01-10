8787f4d9e9bade50966170d393d0f145acfbda35
dannyntale10 <dannyntale10@gmail.com>
2023-01-10 10:07:56 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that writes astring to a text file"""
+
+def write_file(filename="", text=""):
+    """A function that writes a string to a tex tfile(UTF8) and
+    and returns the number of characters written
+    """
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
