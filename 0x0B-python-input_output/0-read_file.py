af2b6c8c9f9a86f98277913040fb0e497ca1ffb6
dannyntale10 <dannyntale10@gmail.com>
2023-01-10 09:52:03 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,8 @@
 """Module that reads a text file"""
 
 def read_file(filename=""):
-    """This is a function that reads a text file in UTF-8 and prints it to stdout"""
+    """This is a function that reads a text file in 
+    UTF-8 and prints it to stdout"""
 
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
