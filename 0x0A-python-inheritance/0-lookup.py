c235a143b084f17ae8073cb53d9ab86c3c2a52c9
dannyntale10 <dannyntale10@gmail.com>
2023-01-11 06:49:57 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Amodule that returns alist"""
+"""Amodule that returns alist."""
 
 def lookup(obj):
     """function that returns the list
         available attributes and methods
         of an object.
     """
-
     return dir(obj)
